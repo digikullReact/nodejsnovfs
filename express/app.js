d814fb0eb3aa57cf86e9d18c70d8d02b34100e4d
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-03-19 11:45:07 +0530

    static files express

--- express/app.js
+++ express/app.js
@@ -3,10 +3,16 @@ const app = express();
 const port = process.env.PORT || 9091;
 
 // For getting the json data from post request
+// application/json in headers
 app.use(express.json()); //
 
 // For getting the form url encoded data in the request
 app.use(express.urlencoded({extended:true}))
+
+
+// express.static
+// this means the express knoww that whatever is inside this folder should be publicly available
+app.use(express.static("static"));
 
 
 app.get("/api", function (req, res, next) {
@@ -38,6 +44,45 @@ app.get("/api/json", function (req, res) {
 
   })
 
+///  -----> HTMl pages
+// M -- V ----> Controller --->
+
+app.get("/html",(req,res)=>{
+  // We have to send html file
+ // console.log(__dirname); //  gives you the absolutre path of the current directory in which you are file  //global variable ../views
+
+ // res.send("hello")
+ //res.sendFile("../views/app.html") // relative path
+ //console.log(__dirname+"/views/app.html");
+
+res.sendFile(__dirname+"/views/app.html")  ///
+
+})
+
+
+
+
+
+
 app.listen(port, () => {
   console.log("Server Running At Port " + port);
 });
+
+
+
+
+
+
+
+// Next session
+
+// Sending html and 
+//template handlebars and the ejs template engiene
+// Creating crud api with static data
+
+// relational db//non relational db
+// Intro to mongodb ---> mongoose
+
+
+// Mysql sequeleize orm as well 
+
